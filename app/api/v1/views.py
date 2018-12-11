c654b0f3201e8f417896405ea0ed33ac990467c2
Kyppy Simani <simanikyppy@gmail.com>
2018-12-11 11:20:14 +0300

    #162554468 Update endpoint unit tests

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -10,24 +10,32 @@ db.create_tables()
 
 class Interventions(Resource):
     def get(self):
-        interventions = db.get_all_interventions()
-        return {"status": 200, "data": interventions}, 200
-
+        intervention = db.get_all_interventions()
+        return {"status": 200, "data": intervention}, 200
+        
     def post(self):
         data = request.get_json(silent=True)
         if data['comment'] is None or data['comment'] is "":
-            return{"message": "Empty comment input"}, 400
-        post_data = (data['id'], data['type'], data['location'], 
-                     data['Images'], data['Videos'], data['comment'])
-        db.insert_intervention(post_data)
-        return{"status": 201, "data": [{"id": data['id'], 
-               "message":"Created intervention record"}]}, 201
+            return{"message": "Incident has missing 'comment' field."}, 400
+        if data['type'] == "Redflag" or data['type'] == "Intervention":
+            post_data = (data['id'], data['type'], data['location'], 
+                         data['Images'], data['Videos'], data['comment'])
+            db.insert_intervention(post_data)
+            return{"status": 201, "data": [{"id": data['id'], 
+                   "message":"Created intervention record"}]}, 201
+        return{"message": "Incident is not "
+               "type 'Redflag' or 'Intervention'."}, 400
 
 
 class Intervention(Resource):
     def get(self, intervention_id):
+        if intervention_id is None or intervention_id is "":
+            return{"message": "No incident id provided"}, 400
         intervention = db.get_intervention(intervention_id)
-        return {"status": 200, "data": intervention}, 200
+        if intervention:
+            return {"status": 200, "data": intervention}, 200
+        return{"message": "No record with id {} exists."
+               .format(intervention_id)}, 404
 
     def delete(self, intervention_id):
         db.delete_record(intervention_id)
@@ -41,7 +49,16 @@ class Signup(Resource):
         username = data["username"]
         password = data["password"]
         email = data["email"]
-        if username is None or password is None or email is None:
+        firstname = data["firstname"]
+        lastname = data["lastname"]
+        if firstname is None or firstname is "":
+            return {"message": "Please include your "
+                    "firstname and try again."}, 400
+        if lastname is None or lastname is "":
+            return {"message": "Please include your "
+                    "lastname and try again."}, 400
+        if username is None or username is "" or password is None or password\
+                is ""or email is None or email is "":
             return {"message": "Missing signup parameters.Please check your" 
                     "username,password or email and try again"}, 400
         valid = db.authorise_signup(username, password, email)
@@ -51,7 +68,7 @@ class Signup(Resource):
                          data['othername'], email, data['phoneNumber'], 
                          username, password, data['isAdmin'])
             db.insert_user(post_data)
-            return{"status": 201, "data": 
+            return{"status": 201, "data":
                    [{"token": access_token, "user": data}]}, 201
         return {"message": "Bad credentials.Signup failed"}, 400
 
@@ -61,13 +78,14 @@ class Login(Resource):
         data = request.get_json(silent=True)
         username = data["username"]
         password = data["password"]
-        if username is None or password is None:
-            return {"message": "Missing login parameters.Please check your" 
-                    "username or password and try again"}, 400
+        if username is None or username is "" or\
+           password is None or password is "":
+            return {"message": "Missing login parameters.Please check your "
+                    "username or password and try again."}, 400
         valid = db.authorise_login(username, password)
         if valid:
             access_token = create_access_token(identity=password)
-            return{"status": 200, "data": 
+            return{"status": 200, "data":
                    [{"token": access_token, "user": data}]}, 200
         return {"message": "Bad credentials.Login failed"}, 400
 
@@ -104,17 +122,17 @@ class Interventionstatus(Resource):
     def patch(self, intervention_id):
         data = request.get_json(silent=True)
         admin = data["isAdmin"]
-        if admin == 'False':
+        if admin is False:
             return {"message": "You do not have permission " 
                     "to access this route."}, 403
         incident_type = data['type']
         if incident_type != 'Intervention':
             return {"message": "Invalid incident type. "
-                    "Please select an incident of type 'Intervention'"}, 400
+                    "Please select an incident of type 'Intervention.'"}, 400
         status = data["status"]
-        if status is None or status is "":
-            return {"message": "Missing update information"
-                    "check your input and try again"}, 400
+        if status != 'Resolved' or status != 'Rejected':
+            return {"message": "Invalid update information"
+                    "check your input and try again."}, 400
         patch_data = (status, intervention_id)
         db.update_intervention_status(patch_data)
         return{"status": 200, "data":
@@ -126,21 +144,21 @@ class Redflagstatus(Resource):
     def patch(self, red_flag_id):
         data = request.get_json(silent=True)
         admin = data["isAdmin"]
-        if admin == 'False':
-            return {"message": "You do not have permission " 
+        if admin is False:
+            return {"message": "You do not have permission "
                     "to access this route."}, 403
         incident_type = data['type']
         if incident_type != 'Redflag':
             return {"message": "Invalid incident type. "
-                    "Please select an incident of type 'Redflag'"}, 400
+                    "Please select an incident of type 'Redflag'."}, 400
         status = data["status"]
-        if status is None or status is "":
-            return {"message": "Missing update information"
-                    "check your input and try again"}, 400
+        if status != 'Resolved' or status != 'Rejected':
+            return {"message": "Invalid update information"
+                    "check your input and try again."}, 400
         patch_data = (status, red_flag_id)
         db.update_redflag_status(patch_data)
         return{"status": 200, "data":
-               [{"id": red_flag_id, "message": 
+               [{"id": red_flag_id, "message":
                  "Updated redflag record status"}]}, 200
 
 
--- app/tests/test_ch3_api.py
+++ app/tests/test_ch3_api.py
@@ -14,6 +14,8 @@ class TestUsers(unittest.TestCase):
         database.create_tables()
         app.testing = True
         self.app = app.test_client()
+        """User info for testing user-related resources"""
+        """Valid signup credentials"""
         self.user1 = {
             "firstname": "John",
             "lastname": "Doe",
@@ -24,15 +26,124 @@ class TestUsers(unittest.TestCase):
             "password": "123",
             "isAdmin": "False"
         }
+        """Bad credentials:Password and username missing"""
+        self.user2 = {
+            "firstname": "John",
+            "lastname": "Doe",
+            "othername": "Jhonny",
+            "email": "Doe@demo.com",
+            "phoneNumber": "07071010",
+            "username": "",
+            "password": "",
+            "isAdmin": "False"
+        }
+        """Bad credentials:Firstname and lastname missing"""
+        self.user3 = {
+            "firstname": "",
+            "lastname": "",
+            "othername": "Jhonny",
+            "email": "Doe@demo.com",
+            "phoneNumber": "07071010",
+            "username": "",
+            "password": "",
+            "isAdmin": "False"
+        }
+        """Incident records for testing incident-related resources"""
+        """Valid "redflag" record"""
         self.incident1 = {
             "id": 100,
+            "type": "Intervention",
+            "location": "100N,50S",
+            "Images": "[Images]",
+            "Videos": "[Videos]",
+            "comment": "Corruption"
+        }
+        """Invalid 'redflag' record.Incident 'id' missing."""
+        self.incident2 = {
+            "id": "",
             "type": "Redflag",
             "location": "100N,50S",
             "Images": "[Images]",
             "Videos": "[Videos]",
             "comment": "Corruption"
         }
-        
+        """Invalid incident record.Incorrect incident 'type'."""
+        self.incident3 = {
+            "id": 100,
+            "type": "red-flag",
+            "location": "100N,50S",
+            "Images": "[Images]",
+            "Videos": "[Videos]",
+            "comment": "Corruption"
+        }
+        """Invalid incident record.Incident 'comment' missing."""
+        self.incident4 = {
+            "id": 100,
+            "type": "red-flag",
+            "location": "100N,50S",
+            "Images": "[Images]",
+            "Videos": "[Videos]",
+            "comment": ""
+        }
+        """Location/comment field for patching."""
+        self.patch1 = {
+            "location": "[Patched Location]",
+            "comment": "[Patched comment]"
+        }
+        """Invalid Location/comment fields for patching."""
+        self.patch2 = {
+            "location": "",
+            "comment": ""
+        }
+        """Valid Location/comment fields for patching."""
+        self.patch2 = {
+            "location": "",
+            "comment": ""
+        }
+        """Invalid 'intervention' status patch.No admin privilege"""
+        self.int_status1 = {
+            "id": 100,
+            "isAdmin": False,
+            "type": 'Intervention',
+            "status": 'Resolved'
+        }
+        """Invalid 'intervention' status patch.Type is 'Redflag'"""
+        self.int_status2 = {
+            "id": 100,
+            "isAdmin": True,
+            "type": 'Redflag',
+            "status": 'Resolved'
+        }
+        """Invalid 'intervention' status patch.Status is invalid"""
+        self.int_status3 = {
+            "id": 100,
+            "isAdmin": True,
+            "type": 'Intervention',
+            "status": 'Solved'
+        }
+        """Invalid 'Redflag' status patch.No admin privilege"""
+        self.red_status1 = {
+            "id": 100,
+            "isAdmin": False,
+            "type": 'Redflag',
+            "status": 'Resolved'
+        }
+        """Invalid 'Redflag' status patch.Type is 'Intervention'"""
+        self.red_status2 = {
+            "id": 100,
+            "isAdmin": True,
+            "type": 'Intervention',
+            "status": 'Resolved'
+        }
+        """Invalid 'Redflag' status patch.Status is invalid"""
+        self.red_status3 = {
+            "id": 100,
+            "isAdmin": True,
+            "type": 'Redflag',
+            "status": 'Solved'
+        }
+
+    """'Signup' resource tests"""
     def test_user_signup(self):
         response = self.app.post('/api/v1/auth/signup', 
                                  data=json.dumps(self.user1), 
@@ -40,7 +151,34 @@ class TestUsers(unittest.TestCase):
         result = json.loads(response.data)
         self.assertEqual(result['status'], 201)
     
-    def test_user_login(self):
+    def test_user_signup_repeated_credentials(self):
+        self.app.post('/api/v1/auth/signup', 
+                      data=json.dumps(self.user1), 
+                      content_type='application/json')
+        response = self.app.post('/api/v1/auth/signup', 
+                                 data=json.dumps(self.user1), 
+                                 content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_user_signup_missing_username_and_password(self):
+        response = self.app.post('/api/v1/auth/signup', 
+                                 data=json.dumps(self.user2), 
+                                 content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_user_signup_missing_firstname_and_lastname(self):
+        response = self.app.post('/api/v1/auth/signup', 
+                                 data=json.dumps(self.user3), 
+                                 content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["message"], "Please include your "
+                         "firstname and try again.")
+    
+    """'Login' Resource tests"""
+    def test_valid_user_login(self):
         self.app.post('/api/v1/auth/signup', 
                       data=json.dumps(self.user1), 
                       content_type='application/json')
@@ -50,30 +188,150 @@ class TestUsers(unittest.TestCase):
         result = json.loads(response.data)
         self.assertEqual(result['status'], 200)
     
+    def test_user_login_missing_username_password(self):
+        response = self.app.post('/api/v1/auth/login',
+                                 data=json.dumps(self.user2),
+                                 content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["message"], "Missing login parameters."
+                         "Please check your username "
+                         "or password and try again.")
+
+    """ 'GET all interventions' resource test"""
     def test_get_interventions(self):
-        self.app.post('/api/v1/interventions', 
-                      data=json.dumps(self.incident1), 
+        self.app.post('/api/v1/interventions',
+                      data=json.dumps(self.incident1),
                       content_type='application/json')
         response = self.app.get('/api/v1/interventions')
         result = json.loads(response.data)
         self.assertEqual(result['status'], 200)
-    
+
+    """ 'GET specific intervention' resource test"""
     def test_get_specific_intervention(self):
         self.app.post('/api/v1/interventions', 
                       data=json.dumps(self.incident1), 
                       content_type='application/json')
         response = self.app.get('/api/v1/intervention/100')
         result = json.loads(response.data)
-        self.assertEqual(result['status'], 200)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(result['data'][0], 100)
     
-    def test_delete_record(self):
+    def test_get_specific_intervention_missing_id(self):
+        response = self.app.get('/api/v1/intervention/100')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 404)
+    
+    """'POST intervention' resource tests"""
+    def test_post_invalid_record_type(self):
+        response = self.app.post('/api/v1/interventions', 
+                                 data=json.dumps(self.incident3), 
+                                 content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["message"], "Incident is not type "
+                                            "'Redflag' or 'Intervention'.")
+    
+    def test_post_valid_record(self):
+        response = self.app.post('/api/v1/interventions', 
+                                 data=json.dumps(self.incident1), 
+                                 content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 201)
+    
+    def test_post_invalid_record_comment(self):
+        response = self.app.post('/api/v1/interventions', 
+                                 data=json.dumps(self.incident4), 
+                                 content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(result["message"], "Incident has "
+                                            "missing 'comment' field.")
+    
+    """'PATCH 'location' and 'comment' tests"""
+    def test_patch_record_with_valid_location(self):
         self.app.post('/api/v1/interventions', 
                       data=json.dumps(self.incident1), 
                       content_type='application/json')
-        response = self.app.delete('/api/v1/intervention/100')
+        response = self.app.patch('/api/v1/interventions/100/location',
+                                  data=json.dumps(self.patch1), 
+                                  content_type='application/json')
         result = json.loads(response.data)
-        self.assertEqual(result['status'], 200)
-
+        self.assertEqual(response.status_code, 200)
+    
+    def test_patch_record_with_invalid_location(self):
+        self.app.post('/api/v1/interventions', 
+                      data=json.dumps(self.incident1), 
+                      content_type='application/json')
+        response = self.app.patch('/api/v1/interventions/100/location',
+                                  data=json.dumps(self.patch2), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_patch_record_with_invalid_comment(self):
+        self.app.post('/api/v1/interventions', 
+                      data=json.dumps(self.incident1), 
+                      content_type='application/json')
+        response = self.app.patch('/api/v1/interventions/100/comment',
+                                  data=json.dumps(self.patch1), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_patch_record_with_valid_comment(self):
+        self.app.post('/api/v1/interventions', 
+                      data=json.dumps(self.incident1), 
+                      content_type='application/json')
+        response = self.app.patch('/api/v1/interventions/100/comment',
+                                  data=json.dumps(self.patch2), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
+    """ 'Intervention status' test"""
+    def test_int_patch_status_no_admin(self):
+        response = self.app.patch('/api/v1/interventions/100/status',
+                                  data=json.dumps(self.int_status1), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 403)
+    
+    def test_int_patch_status_bad_type(self):
+        response = self.app.patch('/api/v1/interventions/100/status',
+                                  data=json.dumps(self.int_status2), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_int_patch_status_bad_status(self):
+        response = self.app.patch('/api/v1/interventions/100/status',
+                                  data=json.dumps(self.int_status3), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
+    """ 'Redflag status' test"""
+    def test_red_patch_status_no_admin(self):
+        response = self.app.patch('/api/v1/red_flags/100/status',
+                                  data=json.dumps(self.red_status1), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 403)
+    
+    def test_red_patch_status_bad_type(self):
+        response = self.app.patch('/api/v1/red_flags/100/status',
+                                  data=json.dumps(self.red_status2), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
+    def test_red_patch_status_bad_status(self):
+        response = self.app.patch('/api/v1/red_flags/100/status',
+                                  data=json.dumps(self.red_status3), 
+                                  content_type='application/json')
+        result = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)
+    
     database.drop_tables()
 
