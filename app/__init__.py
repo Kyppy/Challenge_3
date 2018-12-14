a358f4edfbfd2f3e77c38cabed56c3dcfe1de942
Kyppy Simani <simanikyppy@gmail.com>
2018-12-14 23:15:02 +0300

    #162661049 Apply JWT tokes to all relevant endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, Blueprint
 from flask_jwt_extended import(JWTManager, jwt_required, create_access_token)
 from .api.v1 import version_one as v1
+from instance.config import config
+import os
 
 
 def create_app():
     app = Flask(__name__)
+    # app.config.from_object(config[config_name])
     app.register_blueprint(v1)
-    app.config['JWT_SECRET_KEY'] = 'spongebob'
-    jwt = JWTManager(app)
+    app.config['JWT_SECRET_KEY'] = os.getenv('SECRET')
+    jwt = JWTManager(app)    
     return app
